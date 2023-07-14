8cfc9108efa344a46b6a5ec221ad07e802ad4abc
GabrielZolk <dialixkat@gmail.com>
2023-07-13 22:33:56 -0300

    responsividade mobile: done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,13 @@
 import './Home.css';
+import { useState } from 'react';
 
 export default function Home() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="container">
       <video style={{ width: '100%', position: 'fixed', minHeight: '100vh', objectFit: 'cover', zIndex: '-1' }} autoPlay muted loop id="background-video">
@@ -8,7 +15,7 @@ export default function Home() {
       </video>
       <header className="navbar">
         <span>AUTOFLUX</span>
-        <nav>
+        <nav className={`menu ${showMenu ? 'show' : ''}`}>
           <ul>
             <li>Start</li>
             <li>Exclusive Cars</li>
@@ -18,6 +25,11 @@ export default function Home() {
           </ul>
           <button>Subscribe</button>
         </nav>
+        <div className='bx-menu' onClick={toggleMenu}>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+        </div>
       </header>
       <div className='mid-content'>
         <h2>OLDSCHOOL + TECHNOLOGY + LUXURY</h2>
